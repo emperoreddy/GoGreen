4c465cab130a38073117cf88bfe533d44d3b6cc0
Serban Corodescu <103532635+SerNamBan@users.noreply.github.com>
2022-10-30 16:57:51 +0200

    implementare scor

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const CHARACTER_POSITION = 1200;
 // var cosLeft =  parseInt(window.getComputedStyle(cos).getPropertyValue("left"));
 // var cosBottom = parseInt(window.getComputedStyle(cos.getPropertyValue("bottom"));
 
-
 // function moveCos(){
 //   cosLeft -= 15;
 //   cos.style.left = cosLeft + "px";
@@ -17,69 +16,84 @@ function click_to_start() {
     var x = e.clientX;
 
     if (x > 400 && x < 2070) {
-    document.getElementById("trash-verde").style.marginLeft =
-      x - CHARACTER_POSITION - 100 + "px";
+      var trashX = (document.getElementById("trash-verde").style.marginLeft =
+        x - CHARACTER_POSITION - 100 + "px");
+    }
+    return trashX;
   };
-}}
+}
 
 const trash_verde = document.getElementById("trash-verde");
-
-// function generateGunoi()
-// {
-//   var pozitie_sticla = 470;
-//   var pozitie_random =  Math.floor(Math.random() * 620);
-//   const sticla = document.createElement("sticla");
-//   sticla.setAttribute("class","sticla");
-//   sticla.appendChild(sticla);
-//   function fallDownGunoi() {
-//     console.log("test");
-//     if(sticlaBottom < trash_verde){
-//       alert("Bravo");
-//     }
-//     sticlaBottom -=10;
-//     sticla.style.bottom = sticlaBottom + "px";
-//   }
-//   setInterval(fallDownGunoi, 20);
-//   setTimeout(fallDownGunoi, 2000);
-//   sticla.style.marginTop = sticlaBottom + "px";
-//   sticla.style.left = sticlaLeft + "px";
-// }
-
-// generateGunoi();
 
 
 function fallTrash() {
   let sticlaTop = document.getElementById("sticla").offsetTop;
-  let trashTop= document.getElementById("trash-verde").offsetTop;
-  const trashHeight= document.getElementById("trash-verde").offsetHeight;
-  const trash = trashTop - trashHeight + 100;
+  let trashTop = document.getElementById("trash-verde").offsetTop;
+  const trashHeight = document.getElementById("trash-verde").offsetHeight;
+  const trash = trashTop - trashHeight + 200;
   const sticlaY = document.getElementById("sticla").offsetLeft;
-  // var pozitie_random =  Math.floor(Math.random() * 620);
-  // const sticla = document.createElement("sticla");
-  // sticla.setAttribute("class","sticla");
-  // sticla.appendChild(sticla);
-  // var sticlaBottom = 0;
-  // var sticlaLeft = pozitie_random;
-  console.log(sticlaY);
+  var pozitie_random = Math.floor(Math.random() * 1900 + 100);
+  let score = document.getElementById("score");
+  const trashWidth = document.getElementById("trash-verde").offsetWidth;
   
+  
+  let i = 1;
   function fallDownGunoi() {
-    if(sticlaTop < trash){
-      sticlaTop +=10;
+    let positionTrash = (document.getElementById("trash-verde").getBoundingClientRect());
+
+    let positionSticla = (document.getElementById("sticla").getBoundingClientRect());
+    console.log(positionTrash.x);
+    console.log(positionSticla.x);
+
+    if (sticlaTop < trash) {
+      sticlaTop += 20;
+      sticlaTop += i;
       sticla.style.top = sticlaTop + "px";
+      i += 0.02;
+
+    } else if (((positionSticla.x <= positionTrash.x && positionSticla.x >= positionTrash.x - trashWidth) || (positionSticla.x >= positionTrash.x && positionSticla.x <= positionTrash.x + trashWidth) ) && (positionSticla.y <= positionTrash.y && positionSticla.y >= positionTrash.y - trashWidth) || (positionSticla.y >= positionTrash.y && positionSticla.y <= positionTrash.y + trashWidth)) {
+      pozitie_random =  Math.floor(Math.random() * 1400 + 200);
+      sticla.style.left = pozitie_random + "px";
+      sticla.style.top = 20 + "px";
+      sticlaTop = 20;
+      sticla.style.top = sticlaTop + "px";
+      score.innerHTML = parseInt(score.innerHTML) + 1;
+
+      if (score.innerHTML == 51) {
+        alert("You win!");
+      } else if (score.innerHTML == 10) {
+        (document.getElementById("copac1").src = "copac2.png");
+        document.getElementById("copac1").style.height = "600px";
+        document.getElementById("copac1").style.width = "600px";
+        document.getElementById("copac1").style.top = "600px";
+        
+      } else if (score.innerHTML == 20) {
+        (document.getElementById("copac1").src = "copac3.png");
+      } else if (score.innerHTML == 30) {
+        (document.getElementById("copac1").src = "copac4.png");
+      } else if (score.innerHTML == 40) {
+        (document.getElementById("copac1").src = "copac5.png");
+      } else if (score.innerHTML == 50) {
+        (document.getElementById("copac1").src = "copac6.png");
       }
     }
-    setInterval(fallDownGunoi, 20);
-    setTimeout(fallDownGunoi, 2000);
-  sticla.style.top = sticlaLeft + "px";
-  
+    else  {alert("You lose! Your score is " + score.innerHTML);
+    sticlaTop = 20;
+  }
+  }
+  setInterval(fallDownGunoi, 20);
+  setTimeout(fallDownGunoi, 2000);
 }
-
 
 fallTrash();
 
+var timer;
+var ele = document.getElementById('timer');
 
-function fall(){
-  
-
-
-}
+(function (){
+  var sec = 0;
+  timer = setInterval(()=>{
+    ele.innerHTML = '00:'+sec;
+    sec ++;
+  }, 1000) 
+})() 
